49b8156f7f236f39d86183c2efe0610042bd269d
tee101 <akpata.thelma@gmail.com>
2024-10-06 21:06:55 +0100

    Project initialization and dependencies installation

--- EvoGym/tailwind.config.js
+++ EvoGym/tailwind.config.js
@@ -5,7 +5,37 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gray-20": "#F8F4EB",
+        "gray-50": "#EFE6E6",
+        "gray-100": "#DFCCCC",
+        "gray-500": "#5Е0000",
+        "primary-100": "#FFE1EO",
+        "primary-300": "#FFAGA3",
+        "primary-500": "#FF6B66",
+        "secondary-400": "#FFCD58",
+      },
+      backgroundImage: (theme) => ({
+        "gradient-yellowred": "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+        "mobile-home": "url('./assets/HomePageGraphic.png')"
+      }),
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"]
+      },
+      content: {
+        evolvetext: "url('./assets/EvolveText-png')",
+        abstractwaves: "url(' ./assets/AbstractWaves.png')",
+        sparkles: "url('•/assets/Sparkles-png')",
+        circes: "url(' ./assets/Circles.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm:"768px",
+      md:"1060px"
+    }
   },
-  plugins: [],
-}
+    plugins: [],
+  }
