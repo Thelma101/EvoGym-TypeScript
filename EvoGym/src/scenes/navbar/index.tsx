5937c8b11ec34fda0215d3c06134d00819adf14d
tee101 <akpata.thelma@gmail.com>
2024-10-07 21:18:41 +0100

    Navbar implementation with React Anchor Link Smooth Scroll

--- EvoGym/src/scenes/navbar/index.tsx
+++ EvoGym/src/scenes/navbar/index.tsx
@@ -3,6 +3,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "./shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 type Props = {
     selectedPage: SelectedPage;
@@ -11,7 +12,9 @@ type Props = {
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center, justify-between";
-  
+  const [isMenuEnabled, setIsMenuEnabled] = useState<boolean>(false);
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
   return (
   <nav>
     <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
@@ -20,34 +23,44 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
         <img src={Logo} alt="logo" />
 
         {/* RIGHT SIDE */}
-        <div className={`${flexBetween} w-full`}>
-          <div className={`${flexBetween} gap-8 text-sm`}>
-            <Link 
-                page="Home" 
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-            />
-            <Link 
-                page="Benefits" 
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-            />
-            <Link 
-                page="Our Classes" 
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-            />
-            <Link 
-                page="Contact Us" 
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-            />
-          </div>
-          <div className={`${flexBetween} gap-8`}> 
-            <p>Sign In</p>
-            <button> Become a Member </button>
-          </div>
-        </div>
+        {
+            isAboveMediumScreens ? (
+                <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <Link 
+                      page="Home" 
+                      selectedPage={selectedPage}
+                      setSelectedPage={setSelectedPage}
+                  />
+                  <Link 
+                      page="Benefits" 
+                      selectedPage={selectedPage}
+                      setSelectedPage={setSelectedPage}
+                  />
+                  <Link 
+                      page="Our Classes" 
+                      selectedPage={selectedPage}
+                      setSelectedPage={setSelectedPage}
+                  />
+                  <Link 
+                      page="Contact Us" 
+                      selectedPage={selectedPage}
+                      setSelectedPage={setSelectedPage}
+                  />
+                </div>
+                <div className={`${flexBetween} gap-8`}> 
+                  <p>Sign In</p>
+                  <ActionButton setSelectedPage= {setSelectedPagez}> Become a Member </ActionButton>
+                </div>
+              </div>
+            ): (
+              <button className="p-2 rounded-full bg-secondary-500"
+                onClick = {() => setIsMenuEnabled(!isMenuEnabled)}
+                >
+                <Bars3Icon className="w-6 h-6 text-white"/>
+              </button>
+            )
+        }
       </div>
     </div>
   </nav>
