f68d8dfb08964f582be4636662df1b91c3f8a6e2
tee101 <akpata.thelma@gmail.com>
2024-10-08 16:25:16 +0100

    NavBar responsiveness update

--- EvoGym/src/scenes/navbar/index.tsx
+++ EvoGym/src/scenes/navbar/index.tsx
@@ -4,6 +4,7 @@ import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "./shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "./shared/ActionButton";
 
 type Props = {
     selectedPage: SelectedPage;
@@ -50,7 +51,9 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                 </div>
                 <div className={`${flexBetween} gap-8`}> 
                   <p>Sign In</p>
-                  <ActionButton setSelectedPage= {setSelectedPagez}> Become a Member </ActionButton>
+                  <ActionButton setSelectedPage= {setSelectedPage}> 
+                    Become a Member 
+                  </ActionButton>
                 </div>
               </div>
             ): (
